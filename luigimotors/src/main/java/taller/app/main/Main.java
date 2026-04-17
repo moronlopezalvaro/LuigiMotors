5721a610948c374da2ac2d17bc40433c9a4b1216
moronlopezalvaro <moronlopezalvaro@gmail.com>
2026-04-17 09:24:43 +0200

    implement Main class to initialize application window and database connection

--- luigimotors/src/main/java/taller/app/main/Main.java
+++ luigimotors/src/main/java/taller/app/main/Main.java
@@ -1,10 +1,24 @@
 package taller.app.main;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import taller.app.controller.ConexionBBDD;
 import taller.app.view.VentanaPrincipal;
 
 public class Main {
     public static void main(String[] args) {
+
         VentanaPrincipal ventana = new VentanaPrincipal();
         ventana.setVisible(true);
+
+        ConexionBBDD conexion = new ConexionBBDD();
+        Connection cn = null;
+        try {
+            cn = conexion.conectar();
+            conexion.insertData();
+            conexion.getData();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
