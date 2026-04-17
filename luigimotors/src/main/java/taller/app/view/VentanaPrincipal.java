0e9d11e902a719ce83f3093842e0c8e4e2e6a9c2
moronlopezalvaro <moronlopezalvaro@gmail.com>
2026-04-17 08:46:56 +0200

    create VentanaPrincipal UI class with initial window configuration

--- luigimotors/src/main/java/taller/app/view/VentanaPrincipal.java
+++ luigimotors/src/main/java/taller/app/view/VentanaPrincipal.java
@@ -1,5 +1,12 @@
 package taller.app.view;
 
-public class VentanaPrincipal {
+import javax.swing.JFrame;
 
+public class VentanaPrincipal extends JFrame {
+    public VentanaPrincipal() {
+        setTitle("Luigi Motors");
+        setSize(500, 1000);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
 }
