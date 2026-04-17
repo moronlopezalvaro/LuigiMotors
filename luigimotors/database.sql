0308cf6aca58a035bb0a63f2bfa2be3749df8c05
moronlopezalvaro <moronlopezalvaro@gmail.com>
2026-04-17 10:41:35 +0200

    refactor some few names that were wrong

--- luigimotors/database.sql
+++ luigimotors/database.sql
@@ -24,7 +24,7 @@ CREATE TABLE reparacion (
 );
 
 -- insertar datos en las tablas
-INSERT INTO Cliente (dni, nombre, telefono, password, rol) VALUES 
+INSERT INTO Cliente (dni, nombre, telefono, contrasenya, rol) VALUES 
 ('12345678A', 'Admin Mecánico', '600111222', 'admin123', 'Administrador'),
 ('87654321B', 'Soraya', '600333444', 'cliente1', 'Cliente'),
 ('11223344C', 'Carlos', '600555666', 'cliente2', 'Cliente');
--- luigimotors/src/main/java/taller/app/controller/ConexionBBDD.java
+++ luigimotors/src/main/java/taller/app/controller/ConexionBBDD.java
@@ -48,7 +48,7 @@ public class ConexionBBDD {
         Connection conexion = conectar();
         try {
             // Datos a insertar
-            String consultasInserccion = "INSERT INTO fabricante VALUES(11, 'Martin');";
+            String consultasInserccion = "INSERT INTO cliente (dni, nombre, telefono, contrasenya, rol) VALUES ('00000000Z', 'Cliente Prueba', '600000000', '1234', 'Cliente');";
             System.out.println(consultasInserccion);
             // Creación del Statement para poder reqalizar la consulta
             Statement consul = conexion.createStatement();
@@ -69,20 +69,22 @@ public class ConexionBBDD {
         if (conexion != null) {
             try {
                 // Datos a consultar
-                String consultasSeleccion = "SELECT * FROM producto";
+                String consultasSeleccion = "SELECT * FROM cliente";
                 System.out.println(consultasSeleccion);
                 Statement consul = conexion.createStatement();
                 // Ejecución de la consulta
                 if (consul.execute(consultasSeleccion)) {
                     ResultSet resultset = consul.getResultSet();
                     while (resultset.next()) {
-                        /*
-                         * Producto producto = new Producto (resultset.getInt("id"),
-                         * resultset.getString("nombre"),
-                         * resultset.getInt("precio"), resultset.getInt("id_fabricante") );
-                         * System.out.println(producto.toString());
-                         */
-
+                        Cliente cliente = new Cliente(
+                                resultset.getInt("id_cliente"),
+                                resultset.getString("dni"),
+                                resultset.getString("nombre"),
+                                resultset.getString("telefono"),
+                                resultset.getString("contrasenya"),
+                                resultset.getString("rol")
+                        );
+                        System.out.println(cliente.toString());
                     }
 
                     System.out.println("Datos recuperados correctamente");
