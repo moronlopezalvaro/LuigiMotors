57c5436388bfe5f2b2cde0bd812f20a201ebdbb8
Manuel1613 <manueljesusarenasramirez@alumnos.ilerna.com>
2026-04-30 11:11:25 +0200

    Corrected window size and scalability

--- luigimotors/src/main/java/taller/app/view/VentanaPrincipal.java
+++ luigimotors/src/main/java/taller/app/view/VentanaPrincipal.java
@@ -5,10 +5,11 @@ import javax.swing.JFrame;
 public class VentanaPrincipal extends JFrame {
     public VentanaPrincipal() {
         setTitle("Luigi Motors");
-        setSize(500, 1000);
+        setSize(500, 950);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        
+        setResizable(false);
+
         Menu menu = new Menu();
         setContentPane(menu);
     }
