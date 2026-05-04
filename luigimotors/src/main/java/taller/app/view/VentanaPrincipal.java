bb86b530ec04cd63315fc223a777b6c7c648e1d7
moronlopezalvaro <moronlopezalvaro@gmail.com>
2026-05-04 11:19:48 +0200

    added app icon

--- luigimotors/src/main/java/taller/app/view/VentanaPrincipal.java
+++ luigimotors/src/main/java/taller/app/view/VentanaPrincipal.java
@@ -1,6 +1,8 @@
 package taller.app.view;
 
 import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import java.net.URL;
 
 public class VentanaPrincipal extends JFrame {
     public VentanaPrincipal() {
@@ -9,6 +11,15 @@ public class VentanaPrincipal extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
+
+        // icono de la aplicación
+        URL iconURL = getClass().getResource("/taller/app/resources/Logo.png");
+        if (iconURL != null) {
+            ImageIcon icon = new ImageIcon(iconURL);
+            setIconImage(icon.getImage());
+        } else {
+            System.err.println("No se pudo encontrar el icono: /taller/app/resources/Logo.png");
+        }
 
         Menu menu = new Menu();
         setContentPane(menu);
